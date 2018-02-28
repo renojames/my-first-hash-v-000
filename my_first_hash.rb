7283dd8e2f7d001c169b5f5c17c10fee7e437ffc
Reno Sorensen <renojames19@gmail.com>
2018-02-28 04:10:41 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,11 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = {
+    "Head Surgeon" => "Hawkeye",
+    "Ferret-Face" => "Burns",
+    "Family Man" => "Hunnicut",
+    "Company Clerk" => "Radar O'Reilly"
+    "Commanding Officer" => "Colonel Potter"
+  }
 
   
 end
